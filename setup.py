e17da1555b9b4a6a3d962455b78482425d8d7117
Mark Lavin <mlavin@caktusgroup.com>
2012-06-28 15:09:32 -0400

    Prep for packaging.

--- setup.py
+++ setup.py
@@ -3,11 +3,9 @@ use_setuptools()
 
 from setuptools import setup, find_packages
 
-# packages = find_packages()
-# packages.remove('sample_project')
 setup(
-    name='rtropo',
-    version='0.0.1',
+    name='rapidsms-tropo',
+    version=__import__('rtropo').__version__,
     author='Caktus Consulting Group',
     author_email='solutions@caktusgroup.com',
     packages=find_packages(),
@@ -17,6 +15,10 @@ setup(
     },
     url='http://github.com/caktus/rapidsms-tropo/',
     license='LICENSE.txt',
-    description='RapidSMS Tropo Backend',
+    description='RapidSMS Tropo Threadless Backend',
     long_description=open('README.rst').read(),
+    install_requires=(
+        'rapidsms-threadless-router>=0.1.1',
+        'tropo-webapi-python>=0.1.1',
+    ),
 )
